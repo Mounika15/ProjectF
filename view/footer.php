a8e599ca3d8e643d2e8caf9379507081a10233b0
mounika guntupalli <mg577@afsconnect1.njit.edu>
2017-05-08 17:50:47 -0400

    footer

---
+++ view/footer.php
@@ -0,0 +1,19 @@
+<html>
+<style>
+.welcome{
+position:bottom;
+background:#4c75af;
+}
+p {
+margin-right: 5px;
+font-family: cursive;
+color: #fff;
+text-align: center;
+}
+</style>
+<body>
+<div class="Welcome">
+<strong> <p> © To Do List.</p></strong>
+<strong> <p> Created By - Mounika Guntupalli</p></strong>
+</div>
+</body>
